2766ff855f30d8b89a7f8acd1f1b5007eec897ad
audixdev <audixdev@gmail.com>
2023-12-25 11:23:21 +0100

    v0.3

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ function saveNote() {
     const noteList = document.getElementById('noteList');
     const newNote = document.createElement('li');
     newNote.textContent = noteContent;
+
+    createRemoveButton(newNote, noteContent);
+
     noteList.appendChild(newNote);
 
     // Save note to local storage
@@ -19,6 +22,31 @@ function saveNote() {
     document.getElementById('noteContent').value = '';
 }
 
+// Function to create a remove button for each note
+function createRemoveButton(newNote, noteContent) {
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.classList.add('removeButton');
+    removeButton.onclick = function () {
+        newNote.remove();
+        removeNoteFromLocalStorage(noteContent);
+    };
+    newNote.appendChild(removeButton);
+}
+
+// Function to remove a note from local storage
+function removeNoteFromLocalStorage(noteContent) {
+    const notes = JSON.parse(localStorage.getItem('notes')) || [];
+    const updatedNotes = notes.filter(note => note !== noteContent);
+    localStorage.setItem('notes', JSON.stringify(updatedNotes));
+}
+
+// Function to remove all notes
+function removeAllNotes() {
+    localStorage.removeItem('notes');
+    document.getElementById('noteList').innerHTML = '';
+}
+
 // Display saved notes on page load
 window.onload = function () {
     const savedNotes = JSON.parse(localStorage.getItem('notes')) || [];
@@ -27,7 +55,10 @@ window.onload = function () {
         const newNote = document.createElement('li');
         newNote.textContent = note;
         noteList.appendChild(newNote);
+
+        createRemoveButton(newNote, note);
     });
 
     document.getElementById('saveNote').addEventListener('click', saveNote);
+    document.getElementById('removeAll').addEventListener('click', removeAllNotes);
 };
